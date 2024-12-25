223eab39e2f36bbf5576457e7d742b053f8f688d
Adam <latifasedyeame@gmail.com>
2024-12-25 12:35:22 -0700

    edit App.js

--- frontend/fit-react/src/App.js
+++ frontend/fit-react/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// imports
 
-function App() {
+// export default = main function
+export default function FishstickApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="FishstickApp">
+      <h1>Welcome to Fishstick!</h1>
     </div>
   );
 }
-
-export default App;
